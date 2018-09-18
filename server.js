0794c1118db791138fc7d9ae4e82bf3933a5bc9b
Suzanne Su <suhysu@gmail.com>
2018-09-18 09:49:29 -0700

    added server js info

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const pg = require('pg');
+const express = require('express');
+const ejs = require('ejs');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+const conString = process.env.DATABASE_URL
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', error => {
+  console.error(error);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}!`)
+});
