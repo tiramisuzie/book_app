e01655f5513d43902d337c24b80a570eeed75155
Suzanne Su <suhysu@gmail.com>
2018-09-19 10:34:35 -0700

    get one page working

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const pg = require('pg');
 const express = require('express');
 const ejs = require('ejs');
 require('dotenv').config();
@@ -8,12 +7,7 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 
-const conString = process.env.DATABASE_URL
-const client = new pg.Client(conString);
-client.connect();
-client.on('error', error => {
-  console.error(error);
-});
+const books = require('./book');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -21,26 +15,10 @@ app.use(express.static('./public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/books', (req, res) => {
-  client.query ('SELECT * FROM books;')
-  .then (result => {
-    res.render('index', {
-      bookTitle: 'Bookster',
-      books: result.rows
-    })
-  })
-  .catch(err => {
-    console.log(err);
-  });
-});
+app.get('/', (req, res) => res.redirect('/book'));
+app.get('/book', books.getBooks);
 
-app.get('*', (req, res) => {
-  res.statusCode = 404;
-  res.render('error', {
-  error: 404, 
-  url: 'SORRY, BAD URL '
-  });
-});
+app.get('/book/:id', books.getOneBook);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`)
