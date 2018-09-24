462a23075c075f8bd109e10a4bd1f3d90974ce56
Earnest Cain <caintre03@gmail.com>
2018-09-23 20:25:45 -0700

    added my finals

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const books = require('./book');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
-
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.redirect('/book'));
@@ -23,6 +22,18 @@ app.get('/book', books.getBooks);
 
 app.get('/book/:id', books.getOneBook);
 app.post('/book', books.createBook);
+
+app.get('/error', (req, res) => {
+  res.render('pages/error', {
+    error: 404, 
+    url: 'SORRY, BAD URL '
+    });
+});
+
+app.get('*', (req, res) => {
+  console.log('*')
+  res.redirect('/error');
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`)
